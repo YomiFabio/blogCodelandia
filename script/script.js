5c7505a91398528ccbed6566b85cc16594b71787
Fábio <fabio.dev07@gail.com>
2021-10-14 09:40:32 -0300

    primeiro commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 800,
+    reset: true
+});
+
+ScrollReveal().reveal('.bloco1', { delay: 50 });
+ScrollReveal().reveal('.inline', { delay: 100 });
+ScrollReveal().reveal('h2', { delay: 150 });
+ScrollReveal().reveal('p', { delay: 200 });
